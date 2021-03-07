2b29b29eece7531d580d25b1c97cc9772f0987ff
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2021-03-07 19:11:18 +0200

    feat: improve return type inference

--- __tests-tsd__/types.test-d.ts
+++ __tests-tsd__/types.test-d.ts
@@ -1,10 +1,62 @@
-import '../types';
 import { knex } from 'knex';
+import { expectType } from 'tsd';
+import '../types';
+import { ILengthAwarePagination, IBasePagination, IWithPagination } from '../types';
 
 const db = knex({});
-db('table').select('*').paginate({
-  perPage: 10,
-  currentPage: 0,
-  isFromStart: true,
-  isLengthAware: false,
-});
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+(async () => {
+  expectType<IWithPagination<User[]>>(
+    await db('users').select('*').paginate<User[]>({
+      perPage: 10,
+      currentPage: 1,
+      isFromStart: true,
+      isLengthAware: false,
+    })
+  );
+
+  expectType<ILengthAwarePagination>(
+    (
+      await db('users').select('*').paginate({
+        perPage: 10,
+        currentPage: 1,
+      })
+    ).pagination
+  );
+
+  expectType<ILengthAwarePagination>(
+    (
+      await db('users').select('*').paginate({
+        perPage: 10,
+        currentPage: 2,
+        isFromStart: true,
+      })
+    ).pagination
+  );
+
+  expectType<ILengthAwarePagination>(
+    (
+      await db('users').select('*').paginate({
+        perPage: 10,
+        currentPage: 2,
+        isFromStart: false,
+        isLengthAware: true,
+      })
+    ).pagination
+  );
+
+  expectType<IBasePagination>(
+    (
+      await db('users').select('*').paginate({
+        perPage: 10,
+        currentPage: 2,
+      })
+    ).pagination
+  );
+})();
--- types.d.ts
+++ types.d.ts
@@ -1,30 +1,42 @@
 import { Knex } from 'knex';
 
 interface IPaginateParams {
-  perPage: number,
-  currentPage: number,
-  isFromStart?: boolean,
-  isLengthAware?: boolean,
+  perPage: number;
+  currentPage: number;
+  isFromStart?: boolean;
+  isLengthAware?: boolean;
 }
 
-interface IWithPagination<T = any> {
-  data: T;
-  pagination: IPagination;
+interface IWithPagination<Data, TParams = IPaginateParams> {
+  data: Data;
+  pagination: IPagination<TParams>;
 }
 
-interface IPagination {
-  total?: number;
-  lastPage?: number;
+type IPagination<TParams> = TParams extends
+  | { currentPage: 1 }
+  | { isFromStart: true }
+  | { isLengthAware: true }
+  ? ILengthAwarePagination
+  : IBasePagination;
+
+interface IBasePagination {
   currentPage: number;
   perPage: number;
   from: number;
   to: number;
 }
 
+interface ILengthAwarePagination extends IBasePagination {
+  total: number;
+  lastPage: number;
+}
+
 declare module 'knex' {
   namespace Knex {
     interface QueryBuilder {
-      paginate<TResult = any[]>(params: IPaginateParams): Knex.QueryBuilder<any, IWithPagination<TResult>>;
+      paginate<TData = any[], TParams extends IPaginateParams = IPaginateParams>(
+        params: Readonly<TParams>
+      ): Knex.QueryBuilder<any, IWithPagination<TData, TParams>>;
     }
   }
 }
