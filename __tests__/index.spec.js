171de3c963690bdae2ac06b79ea2165d8fdb280d
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-10-31 23:44:36 +0200

    Change tests to use mysql2

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -25,7 +25,7 @@ function fakeOracleColumnNameMapping(obj) {
 }
 
 const db = knex({
-  client: 'mysql',
+  client: 'mysql2',
   connection: {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
